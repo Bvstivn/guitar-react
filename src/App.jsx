9ccc410c7b3acaaacd3846bfc12d46288643b53d
bvstivn <bbmmllvv@gmail.com>
2025-12-22 20:30:39 -0300

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+//React
+import { useState } from "react";
+//Componets
+import Header from "./components/Header.jsx";
+import Guitar from "./components/Guitar.jsx";
+
+function App() {
+  //State
+  const [auth, setAuth] = useState(false);
+  const [total, setTotatl] = useState(0);
+  const [cart, setCart] = useState([]);
+
+  return (
+    <>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          <Guitar />
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
